70d801860f753e64ee3964e195599df02666ae47
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-05-06 02:47:39 +0700

    final update before testing

--- controllers/task.js
+++ controllers/task.js
@@ -12,7 +12,7 @@ const getAllTasks = asyncWrapper(async (req, res) => {
 
 const getTask = asyncWrapper(async (req, res, next) => {
     var searchedID = req.params.id
-    var searchedTask = await Task.findOne({'_id': searchedID})
+    var searchedTask = await Task.findOne({'_id': searchedID});
     if(!searchedTask) {
         return next(createCustomError(`Can\'t find task with id of ${searchedID}`, 404))
     }
@@ -22,31 +22,47 @@ const getTask = asyncWrapper(async (req, res, next) => {
     })
 })
 
-const createTask = (req, res) => {
+const createTask = asyncWrapper(async (req, res, next) => {
     var body = req.body
     if(!body.taskName) {
-        return 'error' // customerError
+        return next(createCustomError('Please input task name', 400))
     }
-    var createdTask = Task.create(body)
+    var taskData = {
+        taskName: body.taskName, 
+        description: body.description, 
+        completed: body.completed
+    }
+    Task.create(taskData)
     res.status(201).json({
         success: true, 
-        data: createdTask
+        task: taskData
     })
-}
+})
 
-const deleteTask = (req, res) => {
-    res.status(200).json({
-        success: true, 
-        data: req.body
-    })
-}
-
-const updateTask = (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
+    var deletedID = req.body._id
+    var deletedTask = await Task.findOneAndDelete({'_id': deletedID})
+    if(!deletedTask) {
+        return next(createCustomError(`Can\'t find task with id of ${deletedID}`, 404))
+    }
     res.status(200).json({
         sucess: true, 
-        data: req.body
+        task: null
     })
-}
+})
+
+const updateTask = asyncWrapper(async (req, res, next) => {
+    var updatedID = req.body._id
+    var newData = req.body.data
+    var updatedTask = await Task.findOneAndUpdate({'_id': updatedID}, newData, {runValidators: true})
+    if(!updateTask) {
+        return next(createCustomError(`Can\'t find task with id of ${deletedID}`, 404))
+    }
+    res.status(200).json({
+        sucess: true, 
+        task: updatedTask
+    })
+})
 
 module.exports = {
     getAllTasks, 
